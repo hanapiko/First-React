36b5adc6f3311c2ed557e3d30a32faa4e88a400a
hanapiko <apikojuma94@gmail.com>
2025-05-28 05:57:28 +0300

    composibility in react

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-ReactDOM.render(<h1>My First React</h1>, document.getElementById("root"))
+// ReactDOM.render(<h1>My First React</h1>, document.getElementById("root"))
 // ReactDOM.render(<p>Hello world!, my name is Hannah</p>, document.getElementById("root"))
+function Maincontent(){
+    return (
+        <h1>I'm learning React</h1>
+    )
+}
+ReactDOM.render(<Maincontent />, document.getElementById("root"))
